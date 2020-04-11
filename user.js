4551247e355953c5de3727ecdbe10d95c1389a43
SarahKayMo <sarah.kay.morris@gmail.com>
2020-04-10 22:25:47 -0700

    Add function to include getInitials

--- user.js
+++ user.js
@@ -1,7 +1,20 @@
 const users = require('./users.js');
 
 function sayHello(user) {
-  console.log(`Hello, my name is ${user.firstName} ${user.lastName}. People call me ${user.nickName}. I am ${user.activity}.`);
+  const phrase = `Hello, my name is ${user.firstName} ${user.lastName}. People call me ${user.nickName}. I am ${user.activity}.`
+  console.log(phrase);
+  return phrase;
 }
 
-users.forEach(user => sayHello(user));
+function getInitials(user) {
+  const initials = `${user.firstName[0]}${user.lastName[0]}`;
+  console.log(initials);
+  return initials;
+}
+
+//users.forEach(user => getInitials(user));
+// getInitials(users[1]);
+users.forEach((user) => {
+  sayHello(user);
+  getInitials(user);
+}); 
--- users.js
+++ users.js
@@ -1,14 +1,18 @@
 const users = [
   {
+    id: 12182,
     firstName: 'Maxwell',
     lastName: 'Mears',
     nickName: 'DJ Max',
+    phoneNumber: '7757426305',
     activity: 'teaching Sarah how to code',
   },
   {
+    id: 406,
     firstName: 'Sarah',
     lastName: 'Morris',
     nickName: 'Smo',
+    phoneNumber: '9492442781',
     activity: 'learning about coding',
   }
 ];
